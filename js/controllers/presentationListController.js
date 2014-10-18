e689ab26fe9a4a5dd915f88ca895ca2d32473355
RemadaG <remadag@gmail.com>
2014-10-18 22:05:16 +0200

    voting on presentation list

--- js/controllers/presentationListController.js
+++ js/controllers/presentationListController.js
@@ -1,26 +1,67 @@
-geeconControllers.controller('presentationListController', ['$scope', '$http', '$state', '$sce',
-    function ($scope, $http, $state, $sce) {
+geeconControllers.controller('presentationListController', ['$scope', '$http', '$state', '$sce', '$localStorage',
+    function ($scope, $http, $state, $sce, $localStorage) {
         $scope.presentations = [];
         $scope.searchText = "";
+        $scope.orgPresentations = [];
+
+        $scope.$storage = $localStorage;
+        if ($scope.$storage.votedPresentations === undefined) {
+            $scope.$storage.votedPresentations = [];
+        }
 
         function onInit() {
             $http.get('http://registration.geecon.cz/rest/presentations').
                 success(function (result) {
-                    for (var i in result) {
-                        var presentation = result[i];
-                        var presentationToAdd = {
-                            title: presentation.title,
-                            summary: $sce.trustAsHtml(presentation.summary),
-                            speakers: presentation.speakers
-                        };
-                        $scope.presentations.push(presentationToAdd);
-                    }
+                    $scope.orgPresentations = result;
+                    preparePresentationList(result);
                 }).
                 error(function (data, status, headers, config) {
                     console.warn("Cannot get presentation!")
                 });
-
         }
+
+        function preparePresentationList(presentationListFromService) {
+            $scope.presentations = [];
+            for (var i in presentationListFromService) {
+                var presentation = presentationListFromService[i];
+                var presentationToAdd = {
+                    id: presentation.id,
+                    title: presentation.title,
+                    summary: $sce.trustAsHtml(presentation.summary),
+                    speakers: presentation.speakers
+                };
+                presentationToAdd.voted = isPresentationIsVoted(presentation.id);
+                $scope.presentations.push(presentationToAdd);
+            }
+        }
+
+        function isPresentationIsVoted(presentationId) {
+            var isVoted = false;
+            var votedPresentations = $scope.$storage.votedPresentations;
+            for (var i = 0; i < votedPresentations.length; i++) {
+                if (votedPresentations[i] == presentationId) {
+                    isVoted = true;
+                    break;
+                }
+            }
+            return isVoted;
+        }
+
+        $scope.ratePresentation = function (rate, presentationId) {
+            var addToArray = true;
+            var votedPresentations = $scope.$storage.votedPresentations;
+            for (var i = 0; i < votedPresentations.length; i++) {
+                if (votedPresentations[i] == presentationId) {
+                    addToArray = false;
+                    break;
+                }
+            }
+            if (addToArray) {
+                votedPresentations.push(presentationId);
+            }
+            preparePresentationList($scope.orgPresentations);
+        };
+
 
         onInit();
     }
