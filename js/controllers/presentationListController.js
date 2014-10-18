11458e8a3d0268e71c7e861a447968d704dfb2a8
RemadaG <remadag@gmail.com>
2014-10-18 10:17:35 +0200

    presentations added

--- js/controllers/presentationListController.js
+++ js/controllers/presentationListController.js
@@ -1,9 +1,25 @@
-geeconControllers.controller('presentationListController', ['$scope', '$http', '$state',
-    function ($scope, $http, $state) {
+geeconControllers.controller('presentationListController', ['$scope', '$http', '$state', '$sce',
+    function ($scope, $http, $state, $sce) {
         $scope.presentations = [];
+        $scope.searchText = "";
 
         function onInit() {
-            console.log("TODO: get presentations")
+            $http.get('http://registration.geecon.cz/rest/presentations').
+                success(function (result) {
+                    for (var i in result) {
+                        var presentation = result[i];
+                        var presentationToAdd = {
+                            title: presentation.title,
+                            summary: $sce.trustAsHtml(presentation.summary),
+                            speakers: presentation.speakers
+                        };
+                        $scope.presentations.push(presentationToAdd);
+                    }
+                }).
+                error(function (data, status, headers, config) {
+                    console.warn("Cannot get presentation!")
+                });
+
         }
 
         onInit();
